ef859bf6405bb61bbe914f44bb6d66f93eda0928
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-09 19:58:00 +0400

    test
    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import TodoList from './components/TodoList'
-import './App.css'
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
 
-function App() {
- 
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await axios.get('/api/todos');
+      setTodos(res.data);
+    };
+    fetchTodos();
+  }, []);
+
+  const addTodo = async () => {
+    const res = await axios.post('/api/todos', { text });
+    setTodos([...todos, res.data]);
+    setText('');
+  };
+
+  const updateTodo = async (id, completed) => {
+    const res = await axios.put(`/api/todos/${id}`, { completed });
+    setTodos(todos.map(todo => (todo._id === id ? res.data : todo)));
+  };
+
+  const deleteTodo = async id => {
+    await axios.delete(`/api/todos/${id}`);
+    setTodos(todos.filter(todo => todo._id !== id));
+  };
 
   return (
-    <>
-     <TodoList></TodoList>
-    </>
-  )
-}
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => updateTodo(todo._id, !todo.completed)}
+            />
+            {todo.text}
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
