d9ea7a4580fc9f02638208a372e25c27ae45775a
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-09 19:51:09 +0400

    DataBase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import TodoList from './components/TodoList'
 import './App.css'
 
@@ -10,7 +8,6 @@ function App() {
   return (
     <>
      <TodoList></TodoList>
-     
     </>
   )
 }
