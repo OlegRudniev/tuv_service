f37be0dfed5298c8f5e57a0c329399e166cde2fd
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-10 19:38:58 +0400

    refactor

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,7 @@
-import React from 'react';
-import Header from './Header';
-
 const Home = ({ user }) => {
   return (
     <div className="container mx-auto p-4">
-      <Header user={user} handleLogout={() => {}} /> {/* Dummy handleLogout */}
       <h2>Добро пожаловать, {user ? user.username : 'Гость'}!</h2>
     </div>
   );
 };
-
-export default Home;
