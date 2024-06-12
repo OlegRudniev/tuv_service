e110317a0f18aa38564b454c82130496903770bc
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-12 15:25:21 +0400

    Удаление модели Project

--- api/models/Project.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const projectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  category: { type: String, enum: ['Выполнены', 'Выполняются', 'Будущие проекты'], required: true },
-});
-
-const Project = mongoose.model('Project', projectSchema);
-export default Project;
