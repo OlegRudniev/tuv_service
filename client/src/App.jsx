3003b9c0994c519409560d66e9cedc7f519cfec1
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-12 16:13:40 +0400

    Add import for ProjectsPage in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import ProjectsPage from './components/ProjectsPage';
 import NotFound from './components/NotFound';
 
 const App = () => (
