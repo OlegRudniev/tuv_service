82e10c4ee8310278fe22f6468a7842cc22e52007
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-12 15:13:11 +0400

    Добавление недостающего импорта Header в App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import Header from './components/Header';
 import Logout from './components/Logout';
 import Register from './components/Register';
 import TodoList from './components/TodoList';
