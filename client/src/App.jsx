cc3fd2637e679999074169e6e1418996c5b79f12
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-12 14:53:35 +0400

    Добавлен маршрут для компонента Projects в App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Projects from './components/Projects'; // Импортируем новый компонент
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
