643ad0550c9582d3dfe69363c48ef9d1b5322995
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-12 15:37:20 +0400

    Обновление маршрутизации для компонентов Login, Register и Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,22 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
 import Header from './components/Header';
-import Logout from './components/Logout';
+import Login from './components/Login';
 import Register from './components/Register';
-import TodoList from './components/TodoList';
-import TodoDetail from './components/TodoDetail';
+import Home from './components/Home';
 import NotFound from './components/NotFound';
 
-const App = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    window.location.reload(); // Обновляем страницу после выхода
-  };
-
-  return (
-    <Router>
-      <Header user={user} handleLogout={handleLogout} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/todos" element={<TodoList />} />
-        <Route path="/todo/:id" element={<TodoDetail />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
+
