3fe78c3cea0b1887552830f5a61d81ae1c6fc740
GettRichh <107209693+getrichh@users.noreply.github.com>
2024-06-12 15:34:38 +0400

    Обновление компонента Home для отображения приветствия

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import UserHeader from './UserHeader';
 
+const Home = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
 
-const Home = ({ user }) => {
   return (
     <div className="container mx-auto p-4">
-      <h2>Добро пожаловать, {user ? user.username : 'Гость'}!</h2>
+      <UserHeader user={user} />
+      <h1 className="text-2xl font-bold">Welcome, {user ? user.username : 'Guest'}!</h1>
     </div>
   );
 };
 
 export default Home;
+
